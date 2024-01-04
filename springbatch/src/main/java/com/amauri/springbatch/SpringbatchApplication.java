2869ca9a27389c346daae42635bf9abef17a65fd
Amauri <amaurijr1976@hotmail.com>
2024-01-04 09:36:36 -0300

    subida

---
+++ springbatch/src/main/java/com/amauri/springbatch/SpringbatchApplication.java
@@ -0,0 +1,13 @@
+package com.amauri.springbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//ok
+@SpringBootApplication
+public class SpringbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbatchApplication.class, args);
+	}
+
+}
