e6dcec4a954e431e35c69a7f2d506caa6950dc44
Amauri <amaurijr1976@hotmail.com>
2024-01-04 09:43:29 -0300

    subida

--- springbatch/src/main/java/com/amauri/springbatch/SpringbatchApplication.java
+++ springbatch/src/main/java/com/amauri/springbatch/SpringbatchApplication.java
@@ -2,7 +2,7 @@ package com.amauri.springbatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//ok new branch
+//ok new branch 2
 @SpringBootApplication
 public class SpringbatchApplication {
 
